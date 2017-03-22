3262e22299393f7d0fee1c5884abee49671cba12
Anton Klautsan <dukexar@gmail.com>
2017-03-22 09:18:58 +0000

    Data generator

--- clone.py
+++ clone.py
@@ -12,6 +12,7 @@ from keras.layers import Conv2D, MaxPooling2D, Cropping2D
 from keras.layers import Flatten, Dense, Lambda
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
+import sklearn.utils
 
 
 def create_model():
@@ -28,7 +29,14 @@ def create_model():
     return model
 
 
-def train_model(model, X_train, y_train, initial_epoch, epochs, tf_logs_dir,
+def train_model(model,
+                train_generator,
+                validation_generator,
+                train_steps_per_epoch,
+                validation_steps_per_epoch,
+                initial_epoch,
+                epochs,
+                tf_logs_dir,
                 checkpoints_dir):
     tensorboard_callback = TensorBoard(log_dir=tf_logs_dir, histogram_freq=0,
                                        write_graph=True, write_images=False)
@@ -40,31 +48,90 @@ def train_model(model, X_train, y_train, initial_epoch, epochs, tf_logs_dir,
                                           save_best_only=True, mode='max')
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True,
-              nb_epoch=epochs, initial_epoch=initial_epoch,
-              callbacks=[tensorboard_callback, checkpoint_callback])
+    model.fit_generator(train_generator,
+                        steps_per_epoch=train_steps_per_epoch,
+                        epochs=epochs,
+                        initial_epoch=initial_epoch,
+                        validation_data=validation_generator,
+                        validation_steps=validation_steps_per_epoch,
+                        callbacks=[tensorboard_callback, checkpoint_callback])
 
 
-def load_data(root_dir):
+def generate_data(samples, root_dir, batch_size=32):
+    """
+    Generator to lazily load the data.
+    :param samples: pandas.DataFrame
+    :param batch_size: batch size
+    :return: generator
+    """
+
+    def get_filename(filename):
+        return os.path.join(root_dir, 'IMG', os.path.basename(filename))
+
+    output_shape = (160, 320, 3)
+    angle_adj = 0.1
+
+    center_image_idx = 1
+    left_image_idx = 2
+    right_image_idx = 3
+
+    images = np.empty((batch_size,) + output_shape, np.float32)
+    angles = np.empty((batch_size,), np.float32)
+
+    def gen_data(batch_samples, filename_idx, angle_adj):
+        for idx, row in enumerate(batch_samples.itertuples()):
+            images[idx] = skimage.io.imread(get_filename(row[filename_idx]))
+            angles[idx] = row.steering + angle_adj
+
+        yield sklearn.utils.shuffle(images, angles)
+
+    while True:
+        sklearn.utils.shuffle(samples)
+
+        for offset in range(0, len(samples), batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            yield from gen_data(batch_samples, center_image_idx, 0)
+            yield from gen_data(batch_samples, left_image_idx, angle_adj)
+            yield from gen_data(batch_samples, right_image_idx, -angle_adj)
+
+
+def flip_images(batch_generator):
+    for batch_images, batch_angles in batch_generator:
+        images = batch_images.copy()
+        angles = batch_angles.copy()
+        indices = np.random.randint(0, len(batch_images), int(len(batch_images)/2))
+        for idx in indices:
+            images[idx] = np.fliplr(images[idx])
+            angles[idx] = -angles[idx]
+        yield images, angles
+
+
+def preload_data(root_dir):
     driving_log = pd.read_csv(os.path.join(root_dir, 'driving_log.csv'))
-
-    images = []
-    for source_path in driving_log['center']:
-        filename = os.path.basename(source_path)
-        current_path = os.path.join(root_dir, 'IMG', filename)
-        image = skimage.io.imread(current_path)
-        images.append(image)
-
-    X_train = np.array(images)
-    y_train = np.array(driving_log['steering'])
-    return X_train, y_train
+    train, validation = train_test_split(driving_log, test_size=0.2)
+    return train, validation
 
 
 def main():
-    X_train, y_train = load_data('./data')
+    root_dir = './data'
+    batch_size = 32
+
+    train, validation = preload_data(root_dir)
+    train_generator = flip_images(generate_data(train, root_dir, batch_size=batch_size))
+    validation_generator = flip_images(generate_data(validation, root_dir, batch_size=batch_size))
+
     model = create_model()
-    train_model(model, X_train, y_train, initial_epoch=0, epochs=10,
-                tf_logs_dir='./logs', checkpoints_dir='./')
+
+    train_model(model,
+                train_generator,
+                validation_generator,
+                train_steps_per_epoch=len(train)/batch_size,
+                validation_steps_per_epoch=len(validation)/batch_size,
+                initial_epoch=0,
+                epochs=10,
+                tf_logs_dir='./logs',
+                checkpoints_dir='./')
+
     model.save('model.h5')
 
 
